f35d60ffd514f65589a5908b2688e0e971eb5e64
HazemSayad <94578938+HazemSayad@users.noreply.github.com>
2022-01-11 03:04:40 +0300

    Improved script look and behavior
    improved script look and behavior and also used variable names, added comments and sectioned them into an organized sections that will make the development of the script easier in the future

--- ptrn.sh
+++ ptrn.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+## <inputfile> <outputfile> "string"
+
+## input parameters
+inputfile=$1
+outputfile=$2
 phrase=$3
 
+## generated variabls
 lines=$(wc -l $1 | cut -f 1 -d " ")
 
-echo $1 $2 $3 $lines
+## effective script
+echo Creating $lines based on $1 into $2 with the pattern: "$3"
+
 for ((l=0; l<=$lines; l++))
 do
-  echo $phrase >> $2
-  echo $l
+  echo "" > $outputfile
+  echo $phrase >> $outputfile
 done
