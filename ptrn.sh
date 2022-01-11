85353556557133304d40e1bb39f84c6c389108f3
HazemSayad <94578938+HazemSayad@users.noreply.github.com>
2022-01-11 03:13:47 +0300

    fixed typos
    replaced numbered parameter variables to their respective keyword variables

--- ptrn.sh
+++ ptrn.sh
@@ -11,7 +11,7 @@ phrase=$3
 lines=$(wc -l $1 | cut -f 1 -d " ")
 
 ## effective script
-echo Creating $lines based on $1 into $2 with the pattern: "$3"
+echo Creating $lines based on $inputfile into $outputfile with the pattern: "$phrase"
 
 for ((l=0; l<=$lines; l++))
 do
